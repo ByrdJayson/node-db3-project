bda1904e37391cfdefaa28b39b65ad70e6ecd120
Jayson Byrd <jayson_byrd_95@outlook.com>
2022-01-19 16:12:13 -0500

    middleware

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -50,7 +50,15 @@ const validateScheme = (req, res, next) => {
   }
 */
 const validateStep = (req, res, next) => {
+  const { instructions, step_number } = req.body
+  
 
+  if(instructions === undefined || instructions === '' || typeof instructions !== 'string' || !instructions.trim() || typeof step_number !== 'number' || step_number < 0) {
+    const error = { status: 400, message: 'invalid step'}
+    next(error)
+  } else {
+    next()
+  }
 }
 
 module.exports = {
