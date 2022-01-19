c51faad418c1ddd30ceb41704ee54e6ba1d9d733
Jayson Byrd <jayson_byrd_95@outlook.com>
2022-01-19 15:49:01 -0500

    middleware

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,3 +1,5 @@
+const db = require('../../data/db-config')
+
 /*
   If `scheme_id` does not exist in the database:
 
@@ -6,9 +8,21 @@
     "message": "scheme with scheme_id <actual id> not found"
   }
 */
-const checkSchemeId = (req, res, next) => {
+const checkSchemeId = async (req, res, next) => {
+  try{
+    const existing = await db('schemes').where('scheme_id', req.params.scheme_id).first()
 
-}
+    if(!existing){
+      next({status: 404, message: `scheme with scheme_id ${req.params.scheme_id} not found`})
+    } else {
+      next()
+    }
+  } catch(err){
+    next(err)
+  } 
+    
+  }
+
 
 /*
   If `scheme_name` is missing, empty string or not a string:
@@ -19,7 +33,11 @@ const checkSchemeId = (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
-
+  if(req.body.scheme_name === undefined || typeof req.body.scheme_name !== 'string'  || !req.body.scheme_name.trim()) {
+    next({status: 400, message: 'invalid scheme_name'})
+  } else {
+    next()
+  }
 }
 
 /*
